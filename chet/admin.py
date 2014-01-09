703629be787622b05e95f7c5a2e73d6c72610534
Matthias Kestenholz <mk@spinlock.ch>
2014-01-09 21:06:54 +0100

    ModelAdmin classes

--- chet/admin.py
+++ chet/admin.py
@@ -3,22 +3,24 @@ from django.contrib import admin
 from chet.models import Album, Photo
 
 
-admin.site.register(
-    Album,
-    date_hierarchy='date',
-    list_display=('title', 'date', 'is_active', 'is_public'),
-    list_editable=('is_active', 'is_public'),
-    list_filter=('is_active', 'is_public'),
-    prepopulated_fields={'slug': ('title',)},
-    search_fields=('title',),
-)
-admin.site.register(
-    Photo,
-    date_hierarchy='shot_on',
+class AlbumAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('title', 'date', 'is_active', 'is_public')
+    list_editable = ('is_active', 'is_public')
+    list_filter = ('is_active', 'is_public')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title',)
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'shot_on'
     # TODO add thumb
-    list_display=(
-        '__unicode__', 'shot_on', 'is_active', 'is_public', 'is_dark'),
-    list_editable=('is_active', 'is_public', 'is_dark'),
-    list_filter=('is_active', 'is_public', 'is_dark', 'album'),
-    search_fields=('title',),
-)
+    list_display = (
+        '__unicode__', 'shot_on', 'is_active', 'is_public', 'is_dark')
+    list_editable = ('is_active', 'is_public', 'is_dark')
+    list_filter = ('is_active', 'is_public', 'is_dark', 'album')
+    search_fields = ('title',)
+
+
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Photo, PhotoAdmin)
