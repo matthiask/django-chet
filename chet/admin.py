1de55753116da4656628497d9bca52ddf4b68e10
Matthias Kestenholz <mk@spinlock.ch>
2014-01-09 21:22:15 +0100

    Add thumbnails to the admin list

--- chet/admin.py
+++ chet/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from easy_thumbnails.files import get_thumbnailer
+
 from chet.models import Album, Photo
 
 
@@ -16,10 +18,18 @@ class PhotoAdmin(admin.ModelAdmin):
     date_hierarchy = 'shot_on'
     # TODO add thumb
     list_display = (
-        '__unicode__', 'shot_on', 'is_active', 'is_public', 'is_dark')
+        'thumbnail', 'title', 'shot_on', 'is_active', 'is_public', 'is_dark')
     list_editable = ('is_active', 'is_public', 'is_dark')
     list_filter = ('is_active', 'is_public', 'is_dark', 'album')
     search_fields = ('title',)
+
+    def thumbnail(self, photo):
+        options = {'size': (100, 100), 'crop': True}
+        thumb_url = get_thumbnailer(photo.file).get_thumbnail(options).url
+        return '<img src="{0}" alt="">'.format(thumb_url)
+
+    thumbnail.allow_tags = True
+    thumbnail.short_description = 'thumb'
 
 
 admin.site.register(Album, AlbumAdmin)
