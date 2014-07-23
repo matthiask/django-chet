2c1539e2de09c916ec0e369822729e22b64744f7
Matthias Kestenholz <mk@spinlock.ch>
2014-07-23 20:30:30 +0200

    Django 1.7-compatible migrations

--- chet/migrations/0001_initial.py
+++ chet/migrations/0001_initial.py
@@ -1,71 +1,55 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+import easy_thumbnails.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Album'
-        db.create_table(u'chet_album', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('is_public', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('date', self.gf('django.db.models.fields.DateField')(default=datetime.datetime.now)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=200, blank=True)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=200)),
-        ))
-        db.send_create_signal(u'chet', ['Album'])
+    dependencies = [
+    ]
 
-        # Adding model 'Photo'
-        db.create_table(u'chet_photo', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('is_public', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('created_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('file', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
-            ('album', self.gf('django.db.models.fields.related.ForeignKey')(related_name='photos', to=orm['chet.Album'])),
-            ('shot_on', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=200, blank=True)),
-            ('is_dark', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal(u'chet', ['Photo'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Album'
-        db.delete_table(u'chet_album')
-
-        # Deleting model 'Photo'
-        db.delete_table(u'chet_photo')
-
-
-    models = {
-        u'chet.album': {
-            'Meta': {'ordering': "['-date']", 'object_name': 'Album'},
-            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime.now'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '200'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'})
-        },
-        u'chet.photo': {
-            'Meta': {'ordering': "['-created_on']", 'object_name': 'Photo'},
-            'album': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'photos'", 'to': u"orm['chet.Album']"}),
-            'created_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'file': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_dark': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'shot_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['chet']
+    operations = [
+        migrations.CreateModel(
+            name='Album',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('is_active', models.BooleanField(default=True, verbose_name='is active')),
+                ('is_public', models.BooleanField(default=False, verbose_name='is public')),
+                ('created_on', models.DateTimeField(default=django.utils.timezone.now, verbose_name='created on')),
+                ('date', models.DateField(default=django.utils.timezone.now, verbose_name='date')),
+                ('title', models.CharField(max_length=200, verbose_name='title')),
+                ('slug', models.SlugField(unique=True, max_length=200, verbose_name='slug')),
+            ],
+            options={
+                'ordering': [b'-date'],
+                'get_latest_by': b'date',
+                'verbose_name': 'album',
+                'verbose_name_plural': 'albums',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Photo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('is_active', models.BooleanField(default=True, verbose_name='is active')),
+                ('is_public', models.BooleanField(default=False, verbose_name='is public')),
+                ('created_on', models.DateTimeField(default=django.utils.timezone.now, verbose_name='created on')),
+                ('file', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'chet/photos/%Y/%m/', verbose_name='file')),
+                ('shot_on', models.DateTimeField(default=django.utils.timezone.now, verbose_name='shot on')),
+                ('title', models.CharField(max_length=200, verbose_name='title', blank=True)),
+                ('is_dark', models.BooleanField(default=False, help_text='Dark images are shown on a light background.', verbose_name='is dark')),
+                ('album', models.ForeignKey(verbose_name='album', to='chet.Album')),
+            ],
+            options={
+                'ordering': [b'shot_on'],
+                'get_latest_by': b'shot_on',
+                'verbose_name': 'photo',
+                'verbose_name_plural': 'photos',
+            },
+            bases=(models.Model,),
+        ),
+    ]
